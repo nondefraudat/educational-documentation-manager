cd53df9ff9680a6e14fb6a181c9c6136d34665d7
Владислав <60321109+nondefraudat@users.noreply.github.com>
2023-11-01 20:55:03 +0300

    Исправлена аннотация

--- app/doctests.py
+++ app/doctests.py
@@ -1,5 +1,5 @@
 from docxtpl import DocxTemplate
 
-template = DocxTemplate('templates/word/annotation')
+template = DocxTemplate('templates/word/annotation.docx')
 for tag in template.get_undeclared_template_variables():
     print(tag)
